6b401a67b30d389ca056ea1e7e019f1b27b6b643
Leying Guan <lg689@login1.mccleary.ycrc.yale.edu>
2023-12-06 21:09:27 -0500

    rerun coverage_power comparison

--- dsq-experiments_main_job-2023-12-06.sh
+++ dsq-experiments_main_job-2023-12-06.sh
@@ -2,7 +2,7 @@
 #SBATCH --output dsq-experiments_main_job-%A_%3a-%N.out
 #SBATCH --array 0-599
 #SBATCH --job-name dsq-experiments_main_job
-#SBATCH --mem-per-cpu 1g -t 2:00:00 --mail-type ALL
+#SBATCH --mem-per-cpu 3g -t 4:00:00 --mail-type ALL
 
 # DO NOT EDIT LINE BELOW
 /vast/palmer/apps/avx2/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/guan_leying/lg689/project/PairedRegression/PairedRegression/experiments_main_job.txt --status-dir /gpfs/gibbs/project/guan_leying/lg689/project/PairedRegression/PairedRegression
